adc8dd8cf7c3bb1aced1bee8f8152a5af6078312
Paleless <1505672421@qq.com>
2019-04-25 20:15:20 +0800

    +cycle

--- public/src/main.js
+++ public/src/main.js
@@ -1,18 +1,38 @@
 import xs from 'xstream'
 import { run } from '@cycle/run'
-import { makeDOMDriver, h1 } from '@cycle/dom'
+import { makeDOMDriver, h1, div, button, input } from '@cycle/dom'
+import { adapt } from '@cycle/run/lib/adapt'
+import './index.css'
 
-function main(source) {
-    const sinks = {
-        DOM: xs.periodic(1000)
-            .map(i =>
-                h1(`${i} seconds elapsed`))
+function main(sources) {
+    const button_click$ = sources.DOM.select('button').events('click').startWith('')
+    const input_enter$ = sources.DOM.select('input').events('change')
+    const messages = sources.DOM.select('.messages')
+    console.log(messages)
+    //model
+    const input_v$ = input_enter$
+        .map(e => e.target.value)
+        .startWith('')
+    const messages$ = xs.combine(input_v$, button_click$)
+        .map(([v]) => v)
+
+    //view$
+    const vdom$ = messages$
+        .map(v => div('.container', [
+            div('.messages', v),
+            div('.bottom', [
+                input({ type: 'text' }, ''),
+                button('send')
+            ])
+        ]))
+    return {
+        DOM: vdom$
     }
-    return sinks
 }
 
+
 const drivers = {
-    DOM: makeDOMDriver('#app')
+    DOM: makeDOMDriver('#app'),
 }
 
 run(main, drivers)
